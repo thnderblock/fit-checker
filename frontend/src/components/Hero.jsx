4956511e08d805bbd6e9439224fb0a0e1b381546
thnderblock <thnderblock@gmail.com>
2025-01-17 16:24:56 +1100

    Updated components to be responsive to screen

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,9 +1,11 @@
+import shirt from "../images/shirt.png";
+
 const Hero = () => {
   return (
-    <div className="bg-white h-[calc(100vh-3rem)] w-full flex flex-row gap-6 shadow-md">
-      <div className="h-full w-1/2 flex justify-center item-center">
-        <div className="flex w-[500px] h-full justify-center items-center flex-col">
-          <div className="text-[64px] font-bold font-display tracking-tighter leading-tight">
+    <div className="bg-white h-[calc(100vh-3rem)] w-full flex flex-col lg:flex-row gap-6 shadow-md">
+      <div className="h-1/2 lg:h-full lg:w-1/2 flex justify-center item-center">
+        <div className="flex w-[500px] h-full justify-center items-center flex-col lg:mr-0 lg:ml-auto">
+          <div className="text-[64px] font-[900] font-display tracking-[-5px] leading-tight">
             Your Daily Fits, All in One Place
           </div>
           <div className="font-body mt-6 leading-tight">
@@ -14,23 +16,26 @@ const Hero = () => {
           <div className="h-12 w-full mt-6 flex flex-row">
             <a
               href="/"
-              className="h-full w-32 bg-black text-white font-body flex justify-center items-center rounded-full font-bold"
+              className="h-full w-32 bg-sky-600 text-white font-body flex justify-center items-center rounded-full font-bold"
             >
               Sign Up
             </a>
             <a
               href="/"
-              className="h-full w-32 ml-6 border-2 border-black rounded-full flex justify-center items-center cursor-pointer font-bold"
+              className="h-full w-32 ml-6 border-2 border-black rounded-full flex justify-center items-center font-bold"
             >
               Learn More
             </a>
           </div>
         </div>
       </div>
-      <div className="h-full w-1/2 flex justify-center items-center">
-        <div className="w-3/4 h-3/4 bg-slate-200 flex justify-center items-center">
-          placeholder
-        </div>
+
+      <div className="h-1/2 lg:h-full lg:w-1/2 flex justify-center items-center">
+        <img
+          src={shirt}
+          alt="shirt"
+          className="h-full lg:h-3/4 object-contain lg:ml-10 lg:mr-auto"
+        />
       </div>
     </div>
   );
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,20 +1,24 @@
 const NavBar = () => {
   return (
     <div className="sticky top-0 z-100 bg-white">
-      <div className="h-12 w-full shadow-md flex items-center justify-center relative">
+      <div className="h-12 w-full shadow-md lg:flex items-center justify-center relative">
         <a
           href="/"
-          className="w-8 h-8 bg-black rounded-full text-white text-sm absolute left-12"
+          className="w-8 h-8 bg-black rounded-full text-white text-sm absolute left-12 mt-2 lg:mt-0"
         >
           logo
         </a>
-        <div className="font-bold"> fit-checker </div>
-        <div className="gap-12 flex font-body items-center justify-center absolute right-12">
-          <a href="/">Home</a>
-          <a href="/">FAQ</a>
+        <div className="font-bold hidden lg:block"> fit-checker </div>
+        <div className="gap-12 lg:flex font-body items-center justify-center absolute right-12">
+          <a href="/" className="hidden lg:block">
+            Home
+          </a>
+          <a href="/" className="hidden lg:block">
+            FAQ
+          </a>
           <a
             href="/"
-            className="w-24 h-8 flex justify-center items-center rounded-full bg-black text-white font-bold shadow-md"
+            className="w-24 h-8 flex justify-center items-center rounded-full bg-sky-600 text-white font-bold shadow-md mt-2 lg:mt-0"
           >
             Sign Up
           </a>
