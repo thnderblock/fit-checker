10c01b85b699f6c2952284753f8531dbc283b493
Angus Chao <z5358746@unsw.edu.au>
2025-06-04 21:48:41 +1000

    tried to solve large data by down size image

--- backend/database/helper.py
+++ backend/database/helper.py
@@ -1,5 +1,6 @@
 from database import queries as query
 import base64
+from PIL import Image
 
 all_users = query.get_all_users()
 all_clothes = query.get_all_clothes()
@@ -42,7 +43,9 @@ def get_recommendation(username):
 def convert_image_to_base64(image_path):
     with open(image_path, 'rb') as image_file:
         # From https://stackoverflow.com/a/6375973
-        binary_fc = image_file.read()
+        img = image_file.read()
+        new_size = (img.width // 4, img.height // 4)
+        binary_fc = img.resize(new_size, Image.ANTIALIAS)
         base64_utf8_str = base64.b64encode(binary_fc).decode('utf-8')
         ext = image_path.split('.')[-1]
         image_data = f'data:image/{ext};base64,{base64_utf8_str}'
